298e3728606c6869ffa7ecfd5b59ea846e55fcd5
matt <Matthew_Joseph_Hall@hotmail.co.uk>
2023-02-17 03:45:56 +0000

    And Again

--- Installer.sh
+++ Installer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-PACMAN_APPS="ark bluez blueman brightnessctl chromium discord cargo cifs-utils dolphin dunst ffmpegthumbs firefox flatpak font-manager fuse2 gimp gtk2 gtk3 gtk4 kdelibs4support kio kdialog kitty kvantum libwnck3 micro neofetch networkmanager nmap otf-font-awesome packagekit-qt5 plasma-framework powerline-fonts qt5ct rust samba vlc xdg-desktop-portal"
+PACMAN_APPS="ark bluez blueman brightnessctl chromium discord cargo cifs-utils dolphin dunst ffmpegthumbs firefox flatpak font-manager fuse2 gimp gtk2 gtk3 gtk4 kdelibs4support kio kdialog kitty kvantum libwnck3 micro neofetch networkmanager nmap otf-font-awesome packagekit-qt5 plasma-framework plasma-workspace powerline-fonts qt5ct rust samba vlc xdg-desktop-portal"
 PARU_APPS="arcolinux-logout catch2-git hyprland-git nordic-darker-theme nwg-look otf-font-awesome-4 otf-font-awesome-5 overseerr sddm-git sddm-config-editor-git sirula waybar-hyprland-git wev xdg-desktop-portal-hyprland-git"
 
-sudo pacman -Sy $PACMAN_APPS
+sudo pacman -Sy $PACMAN_APPS --noconfirm
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -Sy $PARU_APPS
+paru -Sy $PARU_APPS --noconfirm --sudoloop
 
 sh ~/Arch-Hyprland/sync.sh
 
