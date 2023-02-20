09f7d9df8f1f97b02dea088865f8d654c46efaea
matt <Matthew_Joseph_Hall@hotmail.co.uk>
2023-02-20 13:54:57 +0000

    Getting There

--- HL.sh
+++ HL.sh
@@ -9,10 +9,11 @@ export XCURSOR_SIZE=28
 
 # Wayland stuff
 export MOZ_ENABLE_WAYLAND=1
-export QT_QPA_PLATFORM="wayland;xcb"
+export QT_QPA_PLATFORM=wayland
 export QT_QPA_PLATFORMTHEME=qt5ct
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export CLUTTER_BACKEND=wayland
 
-exec systemd-cat --identifier=hyprland /usr/bin/Hyprland $@
+exec Hyprland
+
--- Installer.sh
+++ Installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PACMAN_APPS="ark bluez blueman brightnessctl chromium discord cargo cifs-utils dolphin dunst ffmpegthumbs firefox flatpak font-manager fuse2 gimp gtk2 gtk3 gtk4 kdelibs4support kio kdialog kitty kvantum libwnck3 micro neofetch networkmanager nmap otf-font-awesome packagekit-qt5 plasma-framework plasma-workspace powerline-fonts qt5ct rust samba vlc xdg-desktop-portal"
+PACMAN_APPS="ark bluez blueman brightnessctl chromium discord cargo cifs-utils dolphin dunst ffmpegthumbs firefox flatpak font-manager fuse2 gimp gtk2 gtk3 gtk4 kdelibs4support kio kdialog kitty kvantum libwnck3 micro neofetch nmap otf-font-awesome packagekit-qt5 plasma-framework plasma-workspace powerline-fonts qt5ct rust samba swaybg vlc xdg-desktop-portal"
 PARU_APPS="arcolinux-logout catch2-git hyprland-git nordic-darker-theme nwg-look otf-font-awesome-4 otf-font-awesome-5 overseerr sddm-git sddm-config-editor-git sirula waybar-hyprland-git wev xdg-desktop-portal-hyprland-git"
 
 pacman -S $PACMAN_APPS --noconfirm
